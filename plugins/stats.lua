26ce82fc64f10e6e680f4d99936217fd94584b24
commanderbotwili <shayanmayeli@yahoo.com>
2015-12-18 13:06:09 +0330

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -141,8 +141,8 @@ return {
     "^[!/]([Ss]tats)$",
     "^[!/]([Ss]tatslist)$",
     "^[!/]([Ss]tats) (group) (%d+)",
-    "^[!/]([Ss]tats) (teleseed)",-- Put everything you like :)
-		"^[!/]([Tt]eleseed)"-- Put everything you like :)
+    "^[!/]([Ss]tats) (satan)",-- Put everything you like :)
+		"^[!/]([Ss]atan)"-- Put everything you like :)
     }, 
   run = run
 }

a42334b69df3705ab14492a291b9400176549c6a
nimaghorbani <ni.gh0098@yahoo.com>
2015-12-21 07:07:10 -0800

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -220,7 +220,7 @@ function create_config( )
     "invite",
     "all"
     },
-    sudo_users = {132921618,169469710},--Sudo users
+    sudo_users = {132921618},--Sudo users
     disabled_channels = {},
     realm = {63974337},--Realms Id
     moderation = {data = 'data/moderation.json'},
